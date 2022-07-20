a6f61380b9e3b8c95c22d978fb226d1f6be655ff
Vincent <cottalorda.vincent@gmail.com>
2022-07-20 03:59:26 +0200

    Renommage NavBar -> Navbar

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,9 +1,9 @@
-import NavBar from "../NavBar/NavBar";
+import Navbar from "../Navbar/Navbar";
 
 const Layout = ({ children }) => {
   return (
     <>
-      <NavBar />
+      <Navbar />
       {children}
     </>
   );
