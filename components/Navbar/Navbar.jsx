ab635501382b8bf06f25b63fde4006d41abcf4d6
Vincent <cottalorda.vincent@gmail.com>
2022-07-20 14:54:45 +0200

    Modif du logo

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import Link from "next/link";
 import styles from "./Navbar.module.css";
+import logo from "../../public/assets/images/logo.png";
 
 const Navbar = () => {
   return (
@@ -7,6 +9,16 @@ const Navbar = () => {
       <Link href="/">
         <a>ComPote</a>
       </Link>
+
+      <Link href="/">
+        <a>
+          <div className={styles.logo}>
+            <Image src={logo} alt="Logo de ComPote" layout="responsive" />
+          </div>
+        </a>
+      </Link>
+
+      <span>A propos</span>
     </nav>
   );
 };
