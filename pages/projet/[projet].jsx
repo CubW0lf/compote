e96b25eccb279fceeb5a53d2dd367f835f3d44ea
Vincent <cottalorda.vincent@gmail.com>
2022-07-25 01:41:53 +0200

    prevent no projet

--- pages/projet/[projet].jsx
+++ pages/projet/[projet].jsx
@@ -30,46 +30,50 @@ export async function getStaticProps(context) {
 const ProjectSingle = ({ projet }) => {
   return (
     <section className={styles.container}>
-      <main>
-        <div className={styles.fimg}>
-          <Image src={getAssetURL(projet.fimg.id)} alt={projet.name} layout="fill" objectFit="cover" />
-        </div>
-        <div className={styles.infos}>
-          <h1>{projet.name}</h1>
-          <div className={styles.tags}>
-            {projet.tools &&
-              projet.tools.map((t) => (
-                <span key={t} className={styles.tag}>
-                  {t}
-                </span>
-              ))}
-          </div>
-          {projet.description && (
-            <div className={styles.description} dangerouslySetInnerHTML={{ __html: projet.description }}></div>
-          )}
-          <a href={projet.url}>
-            <button className={styles.button}>
-              Voir le site <MdLanguage />
-            </button>
-          </a>
-        </div>
-      </main>
-      <div className={styles.mockups}>
-        {projet.mockups &&
-          projet.mockups
-            .sort((a, b) => a.directus_files_id.title - b.directus_files_id.title)
-            .map((m) => (
-              <div className={styles.wrapper} key={m.directus_files_id.id}>
-                <Image
-                  src={getAssetURL(m.directus_files_id.id)}
-                  alt=""
-                  layout="responsive"
-                  width={m.directus_files_id.width}
-                  height={m.directus_files_id.height}
-                />
+      {projet && (
+        <>
+          <main>
+            <div className={styles.fimg}>
+              <Image src={getAssetURL(projet.fimg.id)} alt={projet.name} layout="fill" objectFit="cover" />
+            </div>
+            <div className={styles.infos}>
+              <h1>{projet.name}</h1>
+              <div className={styles.tags}>
+                {projet.tools &&
+                  projet.tools.map((t) => (
+                    <span key={t} className={styles.tag}>
+                      {t}
+                    </span>
+                  ))}
               </div>
-            ))}
-      </div>
+              {projet.description && (
+                <div className={styles.description} dangerouslySetInnerHTML={{ __html: projet.description }}></div>
+              )}
+              <a href={projet.url}>
+                <button className={styles.button}>
+                  Voir le site <MdLanguage />
+                </button>
+              </a>
+            </div>
+          </main>
+          <div className={styles.mockups}>
+            {projet.mockups &&
+              projet.mockups
+                .sort((a, b) => a.directus_files_id.title - b.directus_files_id.title)
+                .map((m) => (
+                  <div className={styles.wrapper} key={m.directus_files_id.id}>
+                    <Image
+                      src={getAssetURL(m.directus_files_id.id)}
+                      alt=""
+                      layout="responsive"
+                      width={m.directus_files_id.width}
+                      height={m.directus_files_id.height}
+                    />
+                  </div>
+                ))}
+          </div>
+        </>
+      )}
     </section>
   );
 };
